94932da3dd7aa3eaea5531b08f884c0dc94e58d4
Adrian Struszczyk <adrianstru@gmail.com>
2019-04-13 18:21:33 -0400

    added blurb, removed HelloWorld from scaffold

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <WebGL/>
+    <div class="blurb">
+      <h2>Hello </h2>
+      <h4>My name is Adrian Struszczyk</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      <p>Itaque earum rerum hic tenetur a sapiente delectus.</p>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import WebGL from '@/components/WebGL.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    WebGL,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+
+}
 </script>
+
+<style scoped>
+    .home {
+      width: 100vw;
+      height: 100vh;
+    }
+
+    .blurb {
+      position: absolute;
+      max-width: 1000px;
+      top: 30%;
+      left: 50%;
+      transform: translate(-50%, -30%);
+    }
+</style>
