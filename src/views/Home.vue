7c7247ca37bdb403f4dee1d29542f66b06177779
Unknown <adrianstru@gmail.com>
2019-04-18 18:28:39 -0400

    added Home as container

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,36 @@
 <template>
   <div class="home">
-    <WebGL/>
-    <div class="blurb">
-      <h2>Hello </h2>
-      <h4>My name is Adrian Struszczyk</h4>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-      <p>Itaque earum rerum hic tenetur a sapiente delectus.</p>
-    </div>
+    <Nav/>
+    <Intro/>
+    <About/>
+    <Work/>
+    <Projects/>
+    <Contact/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import WebGL from '@/components/WebGL.vue';
+import Nav from '@/components/Nav.vue';
+import Intro from '@/components/Intro.vue';
+import About from '@/components/About.vue';
+import Work from '@/components/Work.vue';
+import Projects from '@/components/Projects.vue';
+import Contact from '@/components/Contact.vue';
 
 @Component({
   components: {
-    WebGL,
+    Nav,
+    Intro,
+    About,
+    Work,
+    Projects,
+    Contact,
   },
 })
 export default class Home extends Vue {
-
+  constructor() {
+    super();
+  }
 }
 </script>
-
-<style scoped>
-    .home {
-      width: 100vw;
-      height: 100vh;
-    }
-
-    .blurb {
-      position: absolute;
-      max-width: 1000px;
-      top: 30%;
-      left: 50%;
-      transform: translate(-50%, -30%);
-    }
-</style>
