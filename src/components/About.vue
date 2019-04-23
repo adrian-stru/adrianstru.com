c07d7c64c56787e250939cc31a4b6384e31251a4
Unknown <adrianstru@gmail.com>
2019-04-23 09:29:01 -0400

    factored out content to 'src/data/about'

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,25 +1,15 @@
 <template>
-    <section class="text-white py-32 bg-black w-5/6 px-12 lg:w-max mx-auto" id="about">
-        <div class="w-full">
-            <h1 class="about-header">About Me</h1>
-            <div class="about-inner py-6 w-full lg:w-1/2">
-                <div class="about-blurb text-grey-lightest leading-normal">
-                    <p>Hello! I'm Adrian Struszczyk, a web developer based in Brooklyn, NY. I make websites and I need to hire a copywriter.</p>
-                    <p>I'm one foreign language class away from graduating. 🎓</p>
-                    <p>When I'm not programming I'm banging my head against the wall.</p>
-                    <p>Here's a few things I'm experienced with: </p>
+    <section class="base-grid text-white bg-black" id="about">
+        <div class="w-full lg:w-2/3 lg:mx-auto">
+            <h1 class="about-header text-center sm:text-left">About Me</h1>
+            <div class="about-inner py-6 w-full">
+                <div class="about-inner__blurb text-grey-lightest leading-normal text-left">
+                    <p v-for="(inner, index) in config.paragraphs" :key="index" v-html="inner"/>
                 </div>
-                <ul class="about-skills text-grey-lightest text-sm leading-loose">
-                    <li>- Javscript</li>
-                    <li>- Typescript</li>
-                    <li>- Vue </li>
-                    <li>- React</li>
-                    <li>- Vue </li>
-                    <li>- React</li>
-                    <li>- Vue </li>
-                    <li>- React</li>
-                    <li>- Vue </li>
-                    <li>- React</li>
+                <ul class="about-inner_skills col-count-2 text-grey-lightest text-sm leading-loose">
+                    <li v-for="(skill, index) in config.skills" :key="index">
+                        - {{skill}}
+                    </li>
                 </ul>
             </div>
         </div>
@@ -29,22 +19,26 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import ScrollReveal from 'scrollreveal';
+import {aboutConfig, AboutConfig} from '@/data/about';
 
 @Component
 export default class About extends Vue {
+    private config: AboutConfig;
+
     constructor() {
         super();
+        this.config = aboutConfig;
     }
 
     private mounted() {
         // @ts-ignore
-        ScrollReveal().reveal('.about-blurb, .about-header', {
+        ScrollReveal().reveal('.about-inner__blurb, .about-header', {
             delay: 50,
             cleanup: true,
             distance: '50px',
         });
         // @ts-ignore
-        ScrollReveal().reveal('.about-skills', {
+        ScrollReveal().reveal('.about-inner_skills', {
             delay: 50,
             cleanup: true,
             distance: '150px',
@@ -53,9 +47,3 @@ export default class About extends Vue {
     }
 }
 </script>
-
-<style scoped>
-.about-skills {
-    column-count: 2;
-}
-</style>
