5d77b2e007556d7b82c0313ccf06278ca3696be6
Unknown <adrianstru@gmail.com>
2019-04-23 09:45:13 -0400

    tweaked layout

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,5 +1,5 @@
 <template>
-    <section id="projects" class="text-white py-32 bg-black mx-auto px-12 w-5/6 lg:w-max">
+    <section id="projects" class="base-grid text-white bg-black">
         <div class="w-full">
             <h1 class="text-center">Projects</h1>
             <ProjectManager :projects="projectsData"/>
