6494b9d51991a8b15a06edfe1080a7856a623c8d
Unknown <adrianstru@gmail.com>
2019-04-30 21:49:47 -0400

    updated hover styles and font

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,7 +3,7 @@
         <div class="w-full lg:w-2/3 lg:mx-auto">
             <h1 class="about-header text-center sm:text-left">About Me</h1>
             <div class="about-inner py-6 w-full">
-                <div class="about-inner__blurb text-grey-lightest leading-normal text-left">
+                <div class="about-inner__blurb text-grey-lightest leading-normal text-left font-sans text-xl">
                     <p v-for="(inner, index) in config.paragraphs" :key="index" v-html="inner"/>
                 </div>
                 <ul class="about-inner_skills col-count-2 text-grey-lightest text-sm leading-loose">
@@ -47,3 +47,16 @@ export default class About extends Vue {
     }
 }
 </script>
+
+<style lang="scss">
+    .about-inner {
+        a {
+            color: #8080C0;
+            transition: 0.5s all;
+            position: relative;
+            &:hover {
+                color: #B3B3F3;
+            }  
+        }
+    }
+</style>
