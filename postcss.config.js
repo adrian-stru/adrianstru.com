7a02f847b7e6fe2af018a1e7363c5b3fa32ef9f2
Unknown <adrianstru@gmail.com>
2019-05-01 20:49:51 -0400

    added purgecss & cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,22 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
 const postcssImport = require('postcss-import');
+const purgecss = require('@fullhuman/postcss-purgecss');
+const cssnano = require('cssnano');
 
 module.exports = {
   "plugins": [
     postcssImport,
     tailwindcss('./tailwind.js'),
-    autoprefixer
+    cssnano({
+      preset: 'default',
+    }),
+    process.env.NODE_ENV === "production"? purgecss({
+      content: [
+          "./src/**/*.html", 
+          "./src/**/*.vue"
+      ]
+    }): "",
+    autoprefixer,
   ]
 };
