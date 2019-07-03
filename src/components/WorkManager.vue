4489904ec7ccd1c37d658723771a11b2b59a5e17
Unknown <adrianstru@gmail.com>
2019-07-03 13:36:06 -0400

    updated work nav style

--- src/components/WorkManager.vue
+++ src/components/WorkManager.vue
@@ -10,7 +10,7 @@
         <div class="work-details mx-auto md:pl-8 md:ml-auto">
             <div class="work-details__head py-2 text-center md:text-left">
                 <span class="text-sm sm:text-lg">{{selectedItem.title}}</span>
-                <span class="text-sm sm:text-lg trans text-primary hover:text-primary-lighter company-name font-sans tracking-wide "> @ {{selectedItem.company}}</span>
+                <span class="text-sm sm:text-lg trans text-primary hover:text-primary-lighter company-name font-sans tracking-wide"> @ {{selectedItem.company}}</span>
             </div>
             <div class="work-details__date mb-8 text-grey-lighter text-center md:text-left">
                 <span class="text-sm">{{selectedItem.startDate}} {{(selectedItem.startDate === '') ? '' : '-'}} {{selectedItem.endDate}}</span>
@@ -18,7 +18,7 @@
             <ul class="work-details__list p-0">
                 <li v-for="(bullet, index) in selectedItem.bullets" :key="index"
                 class="bullet pb-3 flex w-full mx-auto lg:m-0 text-left font-sans tracking-wide">
-                    <span class="text-md leading-normal">{{bullet}}</span>
+                    <span class="text-base leading-normal">{{bullet}}</span>
                 </li>
             </ul>
         </div>
@@ -90,5 +90,31 @@ export default class WorkManager extends Vue {
     .work-nav__item--selected {
         background: #232427;
         color: #fff;
+    }
+
+    .work-nav__item {
+        position: relative;
+
+        &::after {
+            content: "";
+            display: block;
+            position: absolute;
+            top: 0;
+            left: 0;
+            height: 2px;
+            width: 100%;
+            background-color: #232427;
+            padding: 0;
+
+            @media only screen and (min-width: 768px) {
+                width: 3px;
+                height: 100%;
+            }
+        }
+
+        &.work-nav__item--selected::after {
+            background-color: #8080c0;
+        }
+
     }
 </style>
