eba4e892c862e8d89c5b0d5635b17076ff9039ce
Unknown <adrianstru@gmail.com>
2019-08-18 10:03:36 -0400

    update data

--- src/data/work.ts
+++ src/data/work.ts
@@ -11,36 +11,38 @@ interface WorkItemInterface {
 
 const workConfig: WorkItemInterface[] = [
         {
-            title: 'Web Developer',
+            title: 'Freelance Developer',
             company: 'Antara Life',
             url: '',
             startDate: 'May 2018',
             endDate: 'February 2019',
             bullets: [
-              'Developed a blog & e-commerce site for a line of herbal supplements',
-              'Wrote custom integration to support product subscriptions and shipping fulfillment between cms and third-party services',
+              'Developed company\'s fully responsive blog & e-commerce shop using Javascript, Vue, and Php',
+              'Wrote custom integration to support product subscriptions and shipping fulfillment between between the content management system and third-party APIs',
               'Simplified technical problems for the designer and the founder to collect requirements and design solutions',
             ],
         },
         {
-            title: 'Front-End Developer',
+            title: 'Freelance Developer',
             company: 'Rove NYC',
             url: '',
             startDate: '',
             endDate: 'Fall 2017',
             bullets: [
-              'Worked with a designer to create a custom front end for Shopify store for a jewelry brand',
+              'Interfaced with a designer to create a custom front end for Shopify store for a jewelry brand using JavaScript, jQuery, HTML, and Sass',
               'Created a progressive web app for in-store use to help customers remember their ring sizes',
             ],
         },
         {
-            title: 'Freelance Web Developer',
+            title: 'Freelance Developer',
             company: 'Hyper House',
             url: '',
             startDate: '',
-            endDate: 'August 2017',
+            endDate: 'Fall 2017',
             bullets: [
-              'Launched microsite for an international art contest for children of Caterpillar employees which received hundreds of submissions across different age groups and thousands of votes',
+              'Built and shipped fully responsive microsite for an international art contest for children of Caterpillar employees',
+              'Developed web application where participants can submit artwork which was vetted by moderators and then voted on',
+              'Project received hundreds of submissions across different age groups and thousands of votes all around the world',
             ],
         },
     ];
